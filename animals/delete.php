130f51819faccc816bc4f62170d4760409de566e
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2025-12-03 12:42:41 +0100

    feat(deleteAnimal):add script to delete animal in sql

---
+++ animals/delete.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-Type: application/json");
+include "../database.php";
+
+
+$Nom = $_POST["Nom"];
+$query = "DELETE FROM animal WHERE Nom = '$Nom'";
+
+if (mysqli_query($conn, $query)) {
+    echo json_encode(["success" => true]);
+} else {
+    echo json_encode(["success" => false, "error" => mysqli_error($conn)]);
+}
+
+mysqli_close($conn);
