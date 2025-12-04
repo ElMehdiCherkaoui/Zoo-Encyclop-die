54bd0fff834e58b547b2131b37bc246c382658a4
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2025-12-04 11:35:44 +0100

    feat(habitats):add script for delet a habbit

---
+++ Habitats/habitatsDelete.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-Type: application/json");
+include "../database.php";
+
+
+$NomHab = $_POST["NomHab"];
+$query = "DELETE FROM habitats WHERE NomHab = '$NomHab'";
+
+if (mysqli_query($conn, $query)) {
+    echo json_encode(["success" => true]);
+} else {
+    echo json_encode(["success" => false, "error" => mysqli_error($conn)]);
+}
